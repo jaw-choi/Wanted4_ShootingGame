1dcf4d4122852f9a73affae9b86b1a3e6b199442
jae <wod6983@gmail.com>
2026-02-05 10:14:35 +0900

    창 크기 변경 cmd 설정에서 해야함

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -11,8 +11,6 @@ using namespace Wanted;
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	//system("mode con: cols=100 lines=50");
-
 
 	Engine engine;
 	engine.SetNewLevel(new GameLevel());
